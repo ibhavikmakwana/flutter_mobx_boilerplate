9467b397270b5c063c9a624a03892e728d5a0ada
ibhavikmakwana <bhavikmakwana43@gmail.com>
2020-04-16 19:30:39 +0530

    :sparkles: get article test
    Signed-off-by: ibhavikmakwana <bhavikmakwana43@gmail.com>

---
+++ test/get_article_test.dart
@@ -0,0 +1,14 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:fluttermobxboilerplate/store/articles/article_store.dart';
+
+void main() {
+  test(
+    'Get Articles test'
+    'Check if Article is not empty',
+    () async {
+      final articlesStore = ArticleStore();
+      await articlesStore.getArticles();
+      expect(articlesStore.articles.basepath.isNotEmpty, true);
+    },
+  );
+}
